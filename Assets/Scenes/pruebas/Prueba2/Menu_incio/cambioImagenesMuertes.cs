85a0610c93f6a3127810b46cbc7e0e9cdbac5d5d
Angel Fontes Peña <angelfontesp23@gmail.com>
2022-11-30 13:45:25 -0600

    cambios de imagenes

--- Assets/Scenes/pruebas/Prueba2/Menu_incio/cambioImagenesMuertes.cs
+++ Assets/Scenes/pruebas/Prueba2/Menu_incio/cambioImagenesMuertes.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class cambioImagenes : MonoBehaviour
+public class cambioImagenesMuertes : MonoBehaviour
 {
     public Image imagenUI;
     bool transicion;
     bool transicion2;
-    bool transicion3;
-    bool transicion4;
     // Start is called before the first frame update
     void Start()
     {
         imagenUI = GameObject.Find("Image").GetComponent<Image>();
         transicion=true;
         transicion2= false;
-        transicion3= false;
-        transicion4= false;
     }
 
     // Update is called once per frame
@@ -31,37 +27,17 @@ public class cambioImagenes : MonoBehaviour
             transicion2=false;
             StartCoroutine("EsperarAudio2");
         }
-        if(transicion3){
-            transicion3=false;
-            StartCoroutine("EsperarAudio3");
-        }
-        if(transicion4){
-            transicion4=false;
-            StartCoroutine("EsperarAudio4");
-        }
     }
 
     IEnumerator EsperarAudio(){
         yield return new WaitForSeconds(8);
-        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/Menu2");
+        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/dead");
         transicion2=true;
     }
 
     IEnumerator EsperarAudio2(){
         yield return new WaitForSeconds(8);
-        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/Menu1");
-        transicion3=true;
-    }
-    
-    IEnumerator EsperarAudio3(){
-        yield return new WaitForSeconds(8);
-        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/Menu3");
-        transicion4=true;
-    }
-    
-    IEnumerator EsperarAudio4(){
-        yield return new WaitForSeconds(8);
-        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/Menu4");
+        imagenUI.sprite = Resources.Load<Sprite>("Imagenes/dead2");
         transicion=true;
     }
 }
